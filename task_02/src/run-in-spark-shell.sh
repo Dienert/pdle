229adfd85e9414f750aa8a6675702b12b08bcfed
Diénert Vieira <dienertalencar@gmail.com>
2022-08-08 04:43:58 +0000

    Adicionando a execução via spark shell

--- task_02/src/run-in-spark-shell.sh
+++ task_02/src/run-in-spark-shell.sh
@@ -1,7 +1,8 @@
-spark-shell
---master spark://master:7077
---num-executors 2
---driver-memory 8G
---executor-memory 8G
---executor-cores 4
+spark-shell \
+--master spark://master:7077 \
+--num-executors 2 \
+--driver-memory 8G \
+--executor-memory 4G \
+--executor-cores 2 \
+--conf spark.dynamicAllocation.enabled=false \
 -i /user_data/projeto/pdle/task_02/src/main/scala/02_lr.scala
